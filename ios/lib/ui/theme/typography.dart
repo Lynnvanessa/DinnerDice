19859444754a279d457df62b47b88c049f84e366
lynnvanessa <lynnvanessa1999@gmail.com>
2023-06-03 15:21:59 +0300

    repeat recommendation removed

---
+++ ios/lib/ui/theme/typography.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTypography {
+  static TextStyle bodyBold({Color? color}) => TextStyle(
+        fontWeight: FontWeight.bold,
+        color: color,
+      );
+
+  static TextStyle bodySemiBold({Color? color}) => TextStyle(
+        fontWeight: FontWeight.w600,
+        color: color,
+      );
+
+  static TextStyle headline({Color? color}) => TextStyle(
+        fontWeight: FontWeight.w900,
+        color: color,
+        fontSize: 24,
+      );
+
+  static TextStyle subHeadline({Color? color}) => TextStyle(
+        fontWeight: FontWeight.bold,
+        color: color,
+        fontSize: 20,
+      );
+}
