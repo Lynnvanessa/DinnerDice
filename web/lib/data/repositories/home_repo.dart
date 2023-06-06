f0868b9c9fa4368a055be3a084237f9cf105fdb7
lynnvanessa <lynnvanessa1999@gmail.com>
2023-06-06 15:55:15 +0300

    replaced google_places package with custom http implement

--- web/lib/data/repositories/home_repo.dart
+++ web/lib/data/repositories/home_repo.dart
@@ -1,17 +1,23 @@
+import 'dart:convert';
 import 'dart:math';
 
+import 'package:diner_dice/data/models/alert.dart';
+import 'package:diner_dice/data/models/nearby_place.dart';
 import 'package:diner_dice/data/repositories/base_repo.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_place/google_place.dart';
 import 'package:location/location.dart' as loc;
 
 import '../../utils/functions.dart';
 
 class HomeRepo extends BaseRepo {
-  List<SearchResult> restaurants = [];
-  SearchResult? selectedRestaurant;
+  List<NearbyPlace> restaurants = [];
+  NearbyPlace? suggestedRestaurant;
   String? nextPageToken;
+  String type = "restaurant";
+  final List<NearbyPlace> _previouslySuggestedRestaurants = [];
+
+  List<Alert> alerts = [];
 
   Future<Position> _getCurrentLocation() async {
     bool serviceEnabled;
@@ -24,7 +30,12 @@ class HomeRepo extends BaseRepo {
       serviceEnabled = await location.requestService();
       if (!serviceEnabled) {
         //Location services are not enabled
-        showToast("Location services are not enabled");
+        alerts.add(
+          Alert(
+            message: "Location services are not enabled",
+            type: AlertType.error,
+          ),
+        );
         return Future.error('Location services are not enabled.');
       }
     }
@@ -38,69 +49,145 @@ class HomeRepo extends BaseRepo {
         // Android's shouldShowRequestPermissionRationale
         // returned true. According to Android guidelines
         // your App should show an explanatory UI now.
+        alerts.add(
+          Alert(
+            message:
+                "The app has been denied permission to access location services.",
+            type: AlertType.error,
+          ),
+        );
         return Future.error('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
       // Permissions are denied forever, handle appropriately.
-      showToast(
-          "The app has been denied permission to access location services.",
-          length: Toast.LENGTH_LONG);
+      alerts.add(
+        Alert(
+          message:
+              "The app has been denied permission to access location services.",
+          type: AlertType.error,
+        ),
+      );
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
     return await Geolocator.getCurrentPosition();
   }
 
-  Future<List<SearchResult>> _getNearbyRestaurants({String? pageToken}) async {
-    final position = await _getCurrentLocation();
-    final googlePlace = GooglePlace(MAPS_API_KEY);
-    final result = await googlePlace.search.getNearBySearch(
-      Location(
-        lat: position.latitude,
-        lng: position.longitude,
-      ),
-      5000,
-      type: "restaurant",
-      opennow: true,
-      pagetoken: pageToken,
-    );
-    if (result == null) {
-      if (pageToken != null) {
-        //retry the fetch
-        return await _getNearbyRestaurants(pageToken: pageToken);
-      } else {
-        showToast("Failed to process the request, please try again");
-      }
-      return [];
-    }
-    nextPageToken = result.nextPageToken;
-
-    if ((result.results == null || result.results?.isEmpty == true) &&
-        pageToken == null) {
-      showToast("No restaurants found");
-      return [];
-    }
-    return result.results ?? [];
-  }
-
   Future<void> getRestaurants() async {
-    selectedRestaurant = null;
-    restaurants = [];
-    nextPageToken = null;
+    suggestedRestaurant = null;
 
-    restaurants = await _getNearbyRestaurants();
+    final fetchedRestaurants = await getNearbyPlaces(pageToken: nextPageToken);
+    final resultsInitiallyEmpty = fetchedRestaurants.isEmpty;
 
-    //get random restaurant from list
-    if (restaurants.isNotEmpty) {
-      final random = Random();
-      final index = random.nextInt(restaurants.length - 1);
-      selectedRestaurant = restaurants[index];
+    // remove duplicates
+    fetchedRestaurants.removeWhere((fetchedRestaurant) =>
+        restaurants.indexWhere((initiallyFetchedRestaurant) =>
+            fetchedRestaurant.placeId == initiallyFetchedRestaurant.placeId) !=
+        -1);
+    restaurants.addAll(fetchedRestaurants);
+
+    /*suggesting a random restaurant */
+    final restaurantSuggestions = [...restaurants];
+    // remove restaurants with similar names even if the vicinity is different
+    final cleanedSuggestions = [...restaurantSuggestions];
+    for (final suggestion in restaurantSuggestions) {
+      while (
+          cleanedSuggestions.where((it) => it.name == suggestion.name).length >
+              1) {
+        cleanedSuggestions.removeWhere((it) => it.name == suggestion.name);
+      }
     }
+    restaurantSuggestions
+      ..clear()
+      ..addAll(cleanedSuggestions);
+
+    // remove initially suggested
+    restaurantSuggestions.removeWhere((suggestion) =>
+        _previouslySuggestedRestaurants.indexWhere(
+            (previouslySuggestedRestaurant) =>
+                previouslySuggestedRestaurant.placeId == suggestion.placeId) !=
+        -1);
+    if (restaurantSuggestions.isEmpty && !resultsInitiallyEmpty) {
+      alerts.add(
+        Alert(
+          message:
+              "All open places have been recommended, the app may show duplicate suggestions onwards",
+          type: AlertType.warning,
+          duration: const Duration(seconds: 7),
+        ),
+      );
+      _previouslySuggestedRestaurants.clear();
+      restaurantSuggestions.addAll(restaurants);
+    }
+
+    // select a random restaurant
+    final randomIndex = Random().nextInt(restaurantSuggestions.length);
+    suggestedRestaurant = restaurantSuggestions[randomIndex];
+    _previouslySuggestedRestaurants.add(suggestedRestaurant!);
   }
 
   Future<void> getMoreRestaurants() async {
-    restaurants.addAll(await _getNearbyRestaurants(pageToken: nextPageToken));
+    if (nextPageToken != null) {
+      restaurants.addAll(await getNearbyPlaces(pageToken: nextPageToken));
+    }
+  }
+
+  void clear() {
+    _previouslySuggestedRestaurants.clear();
+    restaurants.clear();
+    nextPageToken = null;
+    suggestedRestaurant = null;
+  }
+
+  Future<List<NearbyPlace>> getNearbyPlaces({String? pageToken}) async {
+    final position = await _getCurrentLocation();
+    String locationStr = "${position.latitude}%2C${position.longitude}";
+    if (kDebugMode) {
+      locationStr = "54.5181885%2C-1.5675516";
+    }
+    String url =
+        "json?location=$locationStr&radius=5000&type=$type&opennow=opennow&key=$MAPS_API_KEY";
+    if (pageToken != null) {
+      url += "&pagetoken=$pageToken";
+    }
+    try {
+      final response = await get(url);
+      if (response.statusCode == 200) {
+        List<NearbyPlace> fetchedRestaurants = [];
+        final data = jsonDecode(response.body);
+        nextPageToken = data["next_page_token"];
+        final results = data["results"] ?? [];
+        for (var res in results) {
+          try {
+            fetchedRestaurants.add(NearbyPlace.fromJson(res));
+          } catch (e) {
+            if (kDebugMode) {
+              print({"tag": "getNearbyPlaces", "error": e});
+            }
+          }
+        }
+        if (fetchedRestaurants.isEmpty && pageToken == null) {
+          showToast("No restaurants found");
+        }
+        return fetchedRestaurants;
+      } else {
+        alerts.add(
+          Alert(
+            message: "Failed to process the request, please try again",
+            type: AlertType.error,
+          ),
+        );
+      }
+    } catch (e) {
+      alerts.add(
+        Alert(
+          message: "Failed to process the request, please try again",
+          type: AlertType.error,
+        ),
+      );
+    }
+    return [];
   }
 }
